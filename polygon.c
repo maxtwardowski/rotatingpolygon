806314c40db5ec28170865747e466ffe2d6c9e2b
maxtwardowski <twardowskimax@gmail.com>
2018-03-02 15:50:08 +0100

    More meaningful variable names, FPS cap added, backup file removed

--- polygon.c
+++ polygon.c
@@ -11,41 +11,42 @@
 #define ZOOM 1.025
 
 
-int main(int argc, char* argv[]) { 
-    
-    float theta, sidelength;
-    theta = 0;
-    
+int main(int argc, char* argv[]) {
+
+    float rotation_angle, sidelength;
+    rotation_angle = 0;
+
     sidelength = START_EDGE_LENGTH;
-    
+
     if(initGraph()) {
         exit(3);
     }
-    
+
     while(1) {
-        
+
         filledRect(0, 0, screenWidth() - 1, screenHeight() - 1, BLACK);
         int i, x_next, y_next, x_prev, y_prev;
-        
+
         x_prev = START_X1;
         y_prev = START_Y1;
-        
+
         for(i = 0; i <= EDGES; i++) {
-            
-            x_next = sidelength * cos((2 * PI * i + theta) / EDGES) + START_X1;
-            y_next = sidelength * sin((2 * PI * i + theta) / EDGES) + START_Y1;
+
+            x_next = sidelength * cos((2 * PI * i + rotation_angle) / EDGES) + START_X1;
+            y_next = sidelength * sin((2 * PI * i + rotation_angle) / EDGES) + START_Y1;
             if(i != 0)
                 line(x_prev, y_prev, x_next, y_next, WHITE);
             x_prev = x_next;
             y_prev = y_next;
         }
-        
+
         updateScreen();
+        SDL_Delay(1000 / FPS);
         getkey();
         if (isKeyDown(SDLK_RIGHT))
-            theta += ANGLE_STEP;
+            rotation_angle += ANGLE_STEP;
         else if (isKeyDown(SDLK_LEFT))
-            theta -= ANGLE_STEP;
+            rotation_angle -= ANGLE_STEP;
         else if (isKeyDown(SDLK_UP))
             sidelength *= ZOOM ;
         else if (isKeyDown(SDLK_DOWN))
